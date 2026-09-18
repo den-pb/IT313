c40828db143b27c146122bcaba2c618338c47e59
den-pb <denmarkdeleon0901@gmail.com>
2026-09-18 09:47:26 +0800

    updated employee.js

--- mySystem/myfrontend/src/employee.js
+++ mySystem/myfrontend/src/employee.js
@@ -161,16 +161,16 @@ export class Employee extends Component{
  <thead>
  <tr>
  <th>
- EmployeeId
+ Employee Id
  </th>
  <th>
- EmployeeName
+ Employee Name
  </th>
  <th>
  Department
  </th>
  <th>
- DOJ
+ Date of Joining
  </th>
  <th>
  Options
@@ -190,16 +190,29 @@ export class Employee extends Component{
  data-bs-toggle="modal"
  data-bs-target="#exampleModal"
  onClick={()=>this.editClick(emp)}>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
-  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-  <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+ <svg xmlns="http://www.w3.org/2000/svg" width="16"
+height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 
+16 16">
+ <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-
+2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 
+0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-
+.12l6.813-6.814z"/>
+ <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 
+15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 
+1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
  </svg>
  </button>
  <button type="button"
  className="btn btn-light mr-1"
  onClick={()=>this.deleteClick(emp.EmployeeId)}>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
-  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+ <svg xmlns="http://www.w3.org/2000/svg" width="16"
+height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 
+16">
+ <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 
+0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 
+1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 
+5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-
+7a.5.5 0 0 1 1 0z"/>
  </svg>
  </button>
  </td>
@@ -245,18 +258,18 @@ aria-label="Close"
  </div>
  </div>
  <div className="p-2 w-50 bd-highlight">
-<img width="250px" height="250px" alt="Employee"
-  src={PhotoPath+PhotoFileName}/>
+ <img width="250px" height="250px"
+    src={PhotoPath+PhotoFileName}/>
  <input className="m-2" type="file" onChange={this.imageUpload}/>
  </div>
  </div>
- {EmployeeId===0?
+ {EmployeeId==0?
  <button type="button"
  className="btn btn-primary float-start"
  onClick={()=>this.createClick()}
  >Create</button>
  :null}
- {EmployeeId!==0?
+ {EmployeeId!=0?
  <button type="button"
  className="btn btn-primary float-start"
  onClick={()=>this.updateClick()}
@@ -267,6 +280,6 @@ aria-label="Close"
 </div>
 </div>
 </>
-);
-}
+)
+ }
 }
